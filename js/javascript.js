241f453f410c94e515812ad45cd3e46696a7e5b7
victordurco <victordurco@gmail.com>
2021-07-24 17:19:08 -0300

    eneable order button when 3 items selected done

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,34 @@
 
-function select(item, type){
+function validateOrderButton(){
+    let dish = document.querySelector(".dishes .selected");
+    let drink = document.querySelector(".drinks .selected");
+    let dessert = document.querySelector(".desserts .selected");
+
+    if (dish !== null && drink!== null && dessert !== null){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function eneableOrderButton(){
+    let button = document.querySelector(".close-order");
+    button.classList.add("activate-button");
+    button.innerHTML = "Fechar pedido";
+}
+
+function selectItem(item, type){
     type = "."+type;
-    console.log(type);
     let previousChoice = document.querySelector(type+"  .selected");
     if (previousChoice !== null){
        previousChoice.classList.remove("selected");  
     }
    
-
     item.classList.add("selected");
+
+    if(validateOrderButton()){
+        eneableOrderButton();
+    }
 }
+
+
