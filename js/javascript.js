477b8166e76268db0e12a61b065e46e72f66a3c9
victordurco <victordurco@gmail.com>
2021-07-23 23:20:29 -0300

    select one item of each category done

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
 
 function select(item, type){
+    type = "."+type;
     console.log(type);
-    let previousChoice = document.querySelector(".selected");
+    let previousChoice = document.querySelector(type+"  .selected");
     if (previousChoice !== null){
        previousChoice.classList.remove("selected");  
     }
